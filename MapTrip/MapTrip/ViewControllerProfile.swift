7def31d97a27c9c6164b563a275568e2ca6d8525
Kim-Mingyeong <kalsrud0412@gmail.com>
2022-11-01 16:25:27 +0900

    Add profile

--- MapTrip/MapTrip/ViewControllerProfile.swift
+++ MapTrip/MapTrip/ViewControllerProfile.swift
@@ -2,7 +2,7 @@
 //  ViewControllerProfile.swift
 //  MapTrip
 //
-//  Created by 김민경 on 2022/10/28.
+//  Created by 김민경 on 2022/11/01.
 //
 
 import Foundation
@@ -12,14 +12,11 @@ class ViewControllerProfile: UIViewController {
     
     @IBOutlet var sampleImageView: UIImageView!
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        configure()
+        // Do any additional setup after loading the view.
+        sampleImageView.layer.masksToBounds = false
+        sampleImageView.layer.cornerRadius = sampleImageView.frame.height/2
+        sampleImageView.clipsToBounds = true
     }
-    
-    func configure(){
-        sampleImageView.layer.cornerRadius = CGFloat(8)
-    }
-    
 }
