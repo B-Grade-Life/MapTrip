81e457f7302ea49ceb9b8770920125df197798fe
Kim-Mingyeong <kalsrud0412@gmail.com>
2022-11-01 16:25:27 +0900

    Add profile

--- MapTrip/MapTrip/ViewControllerCalendar.swift
+++ MapTrip/MapTrip/ViewControllerCalendar.swift
@@ -10,26 +10,10 @@ import UIKit
 
 class ViewControllerCalendar: UIViewController {
     
-    let imageView: UIImageView = {
-        let sampleImageView = UIImageView()
-        sampleImageView.image = UIImage(named: "sampleImage")
-        //image 표시방법
-        sampleImageView.contentMode = .scaleAspectFit
-        //둥근 테두리
-        sampleImageView.layer.cornerRadius = 150
-        //넘치는 영역 잘라내기
-        sampleImageView.clipsToBounds = true
-        
-        sampleImageView.translatesAutoresizingMaskIntoConstraints = false
-        return sampleImageView
-    }()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.addSubview(imageView)
+        // Do any additional setup after loading the view.
         
-        NSLayoutConstraint.activate([imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor),imageView.widthAnchor.constraint(equalToConstant: 200),imageView.heightAnchor.constraint(equalToConstant: 230),
-                            ])
     }
     
 }
