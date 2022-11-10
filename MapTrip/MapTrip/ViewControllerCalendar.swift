c16647cd180fccba4a7d725fc7aac9eba2c9d98e
Kim-Mingyeong <kalsrud0412@gmail.com>
2022-11-10 22:38:54 +0900

    Add calendar detail

--- MapTrip/MapTrip/ViewControllerCalendar.swift
+++ MapTrip/MapTrip/ViewControllerCalendar.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewControllerCalendar: UIViewController {
 
     @IBOutlet var lblCurrentMonth: UILabel!
+    @IBOutlet var drawLine: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +22,20 @@ class ViewControllerCalendar: UIViewController {
         
         dateFormatter.dateFormat = "MMM"
         lblCurrentMonth.text = dateFormatter.string(from: date as Date)
+        
+        UIGraphicsBeginImageContext(drawLine.frame.size)
+        let context = UIGraphicsGetCurrentContext()!
+        
+        context.setLineWidth(3.0)
+//        context.setFillColor(UIColor.green.cgColor)
+        context.setStrokeColor(red: 99/255, green: 138/255, blue: 102/255, alpha: 1)
+        context.move(to: CGPoint(x: 8, y: 9))
+        context.addLine(to: CGPoint(x: 8, y: 41))
+        context.strokePath()
+        
+        drawLine.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
     }
 
 }
