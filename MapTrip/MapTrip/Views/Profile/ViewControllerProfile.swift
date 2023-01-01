71bffb143ed0ad5347d2fd9a951cddd4c26ec68f
Lee Gyu Hyuk <matda8080@gmail.com>
2023-01-01 17:39:58 +0900

    Update Home, Profile

--- MapTrip/MapTrip/Views/Profile/ViewControllerProfile.swift
+++ MapTrip/MapTrip/Views/Profile/ViewControllerProfile.swift
@@ -5,29 +5,29 @@
 //  Created by 김민경 on 2022/11/01.
 //
 
-import Foundation
 import UIKit
 
+
 class ViewControllerProfile: UIViewController {
-    
     @IBOutlet var sampleImageView: UIImageView!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         sampleImageView.layer.masksToBounds = false
         sampleImageView.layer.cornerRadius = sampleImageView.frame.height/2
         sampleImageView.clipsToBounds = true
-        
+
         tapGasture()
     }
-    
+
     //MARK: -Add TapGasture in ImageView
     func tapGasture(){
         let tap = UITapGestureRecognizer(target: self, action: #selector(imageTapped))
         sampleImageView.isUserInteractionEnabled = true
         sampleImageView.addGestureRecognizer(tap)
     }
+
     @IBAction func imageTapped(){
         let imagePicker = UIImagePickerController()
         imagePicker.sourceType = .photoLibrary
@@ -36,10 +36,24 @@ class ViewControllerProfile: UIViewController {
         present(imagePicker, animated: true)
     }
 }
+
 extension ViewControllerProfile: UIImagePickerControllerDelegate, UINavigationControllerDelegate{
-    
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+    func imagePickerController(
+        _ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]
+    ) {
         sampleImageView.image = info[.originalImage] as? UIImage
         dismiss(animated:true, completion: nil)
+    }
+}
+
+extension UIImage {
+    func toPngString() -> String? {
+        let data = self.pngData()
+        return data?.base64EncodedString(options: .endLineWithLineFeed)
+    }
+  
+    func toJpegString(compressionQuality cq: CGFloat) -> String? {
+        let data = self.jpegData(compressionQuality: cq)
+        return data?.base64EncodedString(options: .endLineWithLineFeed)
     }
 }
